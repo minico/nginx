c4fbf175e3a7be5e64047b3d44de200c9d5e0202
minico <xj.zhang@msn.com>
2016-05-04 14:01:54 +0800

    Added comments to queue;

--- src/core/ngx_queue.h
+++ src/core/ngx_queue.h
@@ -14,6 +14,12 @@
 
 //xjzhang,  用双向循环链表实现了一个queue;
 //queue有头结点，头结点不存放数据；
+//实际上从ngx_queue_s的定义可以看出，所有节点都不包含数据；
+//那么如果用这个queue来阻止某个类型的数据呢？
+//如果某个类型的数据需要使用queue来组织，则需要在该类型中定义一个ngx_queue_t变量；
+//然后将该类型每个实例中的ngx_queue_t变量连接起来即可；
+//那么如何通过ngx_queue_t变量来获取其对应的数据呢？
+//这就需要使用后面定义的ngx_queue_data();
 typedef struct ngx_queue_s  ngx_queue_t;
 
 struct ngx_queue_s {
@@ -103,6 +109,9 @@ struct ngx_queue_s {
     (h)->prev->next = h;
 
 
+//xjzhang, q是一个类型为ngx_queue_t的变量，link是类型type中定义的ngx_queue_t变量，也就是q；
+//offsetof(type, link)取得q在类型type中的地址偏移量；
+//(u_char *) q - offsetof(type, link)获取的便是类型type的实例变量地址；
 #define ngx_queue_data(q, type, link)                                         \
     (type *) ((u_char *) q - offsetof(type, link))
 
